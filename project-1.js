3b14e5904b3e50924f5a6ba0b2f6950bcf458349
Carey Hinoki <carey@shift.com>
2014-08-14 16:41:37 -0700

    feat(problem-1): Added two more answers.

--- project-1.js
+++ project-1.js
@@ -8,10 +8,64 @@ Find the sum of all the multiples of 3 or 5 below 1000.
   function calculate() {
     var sum = 0;
 
-    for(var i = 0; i < 100000; i++) {
-      if(i % 5 === 0 || i % 3 === 0) {
+    for (var i = 0; i < 1000; i++) {
+      if (i % 3 === 0 || i % 5 === 0) {
         sum += i;
       }
+    }
+
+    console.log(sum);
+  }
+
+  console.time('calculate');
+
+  calculate();
+
+  console.timeEnd('calculate');
+}());
+
+(function () {
+  function calculate() {
+    var sum = 0;
+
+    for (var i = 0; i < 1000; i += 3) {
+      sum += i;
+    }
+
+    for (var i = 0; i < 1000; i += 5) {
+      if (i % 3 !== 0) {
+        sum += i;
+      }
+    }
+
+    console.log(sum);
+  }
+
+  console.time('calculate');
+
+  calculate();
+
+  console.timeEnd('calculate');
+}());
+
+(function () {
+  function iterate5(i) {
+    if ((i - 1) % 3 !== 0) {
+      return i + 5;
+    } else {
+      return i + 10;
+    }
+  }
+
+  function calculate() {
+    var sum = 0;
+
+    for (var i = 3; i < 1000; i += 3) {
+      sum += i;
+    }
+
+    for (var i = 5; i < 1000; i = iterate5(i)) {
+      sum += i;
     }
 
     console.log(sum);
